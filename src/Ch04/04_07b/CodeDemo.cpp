75a3f4564d98fcc797aea31d9f10c3bf2ec007cb
ds726 <debangshu.k.saha@gmail.com>
2025-08-09 22:10:41 +0000

    Chapter 4 changes

--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -7,7 +7,15 @@
 #include <complex>
 
 int main(){
-    
+    std::vector<std::complex<double>> points;
+    points.push_back(std::complex<double>(1.5, 2.5));
+    points.push_back(std::complex<double>(3.0, -4.0));
+    points.push_back(std::complex<double>(-5.0, 6.9));
+    points.push_back(std::complex<double>(0.6, -0.3));
+    std::cout << "The first real part: " << points.begin()->real() << std::endl;
+    std::cout << "Imaginary part at index 1: " << points[1].imag() << std::endl;
+    std::cout << "Next to last real: " << prev(points.end(), 2)->real() << std::endl;
+    std::cout << "Last imaginary: " << (points.end() - 1)->imag() << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
